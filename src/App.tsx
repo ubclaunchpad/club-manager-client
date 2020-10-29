5a1fa04de632d23f90da7ed269b1374fa9772c5b
Jed <jed324@gmail.com>
2020-10-29 00:57:09 -0700

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './views/Login';
 import Dashboard from './views/Dashboard';
 import Home from './views/Home';
