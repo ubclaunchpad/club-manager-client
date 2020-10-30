3f1683285fdf4362919277666da4d137af395acb
calhep <callumahepworth@gmail.com>
2020-10-30 13:11:25 -0600

    Fixed dashboard HTML formatting.

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -4,10 +4,8 @@ import DashboardHeader from '../components/DashboardHeader';
 const Dashboard: React.FunctionComponent = () => {
     return (
         <React.Fragment>
-            <h1>
-                Dashboard
-                <DashboardHeader />
-            </h1>
+            <h1>Dashboard</h1>
+            <DashboardHeader />
         </React.Fragment>
     );
 };
