e3bcab258443d6d1b48de82e87230ea3a029294b
LeonardoKamino <leo.kamino@gmail.com>
2020-11-13 21:44:41 -0300

    Added registration form

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './LoginForm.scss';
 
-class LoginForm extends React.Component {
+interface ILoginFormProps {
+    onClick: () => void;
+}
+
+class LoginForm extends React.Component<ILoginFormProps> {
     render(): React.ReactNode {
         return (
             <div className="sign-in-form">
+                <h1 className="is-size-1 has-text-centered py-3">Sign In</h1>
                 <form>
                     <div className="field py-3">
                         <label className="label is-size-6 has-text-weight-light">Email</label>
@@ -20,6 +25,12 @@ class LoginForm extends React.Component {
                     <h1 className="is-size-6 ">OR</h1>
                 </div>
                 <button className="button google-login-button is-size-6">Login with Google</button>
+                <p className="mt-4 is-size-6 has-text-centered">
+                    Don`t have an account?{' '}
+                    <span className="link has-text-weight-medium" onClick={this.props.onClick}>
+                        Create Your account
+                    </span>
+                </p>
             </div>
         );
     }
