c24fe0c7bcec05afeea82a0aae551c99f54a943d
Leonardo Kamino <59890834+LeonardoKamino@users.noreply.github.com>
2020-11-15 08:24:14 -0300

    Implement Sign Up view (#55)
    * Added registration form
    
    * Fixed banner bug
    
    * Changed class name on scss
    
    * Fixed yarn warnings
    
    * Fixed SASS linter warnings

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import './Login.scss';
-import { NavLink } from 'react-router-dom';
 import LoginBackgroudImg from '../images/LoginBackgroudImg.png';
 import LaunchPadBanner from '../components/login/LaunchPadBanner';
 import LoginForm from '../components/login/LoginForm';
+import SignUpForm from '../components/login/SignUpForm';
 
-const Login: React.FunctionComponent = () => {
-    return (
-        <div className="view ">
-            <div className="columns">
-                <div className="column is-5 left-section  is-hidden-touch" style={{ background: 'white' }}>
-                    <img src={LoginBackgroudImg} alt="Background" />
-                </div>
-                <div className="column right-section">
-                    <LaunchPadBanner />
-                    <div className="sign-in">
-                        <h1 className="is-size-1 has-text-centered py-3">Sign In</h1>
-                        <LoginForm />
-                        <p className="mt-4 is-size-6 has-text-centered">
-                            Don`t have an account?{' '}
-                            <NavLink to="/" className="link has-text-weight-medium">
-                                Create Your account
-                            </NavLink>
-                        </p>
+class Login extends React.Component {
+    state = { mode: 'Login' };
+
+    changeMode = (): void => {
+        if (this.state.mode === 'Login') {
+            this.setState({ mode: 'Register' });
+        } else {
+            this.setState({ mode: 'Login' });
+        }
+    };
+
+    renderForm(): React.ReactNode {
+        if (this.state.mode === 'Login') {
+            return <LoginForm onClick={this.changeMode} />;
+        } else {
+            return <SignUpForm onClick={this.changeMode} />;
+        }
+    }
+
+    render(): React.ReactNode {
+        return (
+            <div className="view ">
+                <div className="columns is-vcentered is-desktop">
+                    <div className="column is-5 left-section  is-hidden-touch" style={{ background: 'white' }}>
+                        <img src={LoginBackgroudImg} alt="Background" />
+                    </div>
+                    <div className="column right-section ">
+                        <LaunchPadBanner />
+                        <div className="form">{this.renderForm()}</div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Login;
