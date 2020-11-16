d6ec514a3be0ab2b30a09c5d2e2ef74e2c99f098
Connor Fong <dconnorfong@gmail.com>
2020-11-15 22:30:03 -0800

    Refactored some styles to align with Figma spec

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { Component, Fragment, ReactNode } from 'react';
 import './LoginForm.scss';
 
 interface ILoginFormProps {
     onClick: () => void;
 }
 
-class LoginForm extends React.Component<ILoginFormProps> {
-    render(): React.ReactNode {
+class LoginForm extends Component<ILoginFormProps> {
+    render(): ReactNode {
         return (
-            <div className="sign-in-form">
-                <h1 className="is-size-1 has-text-centered py-3">Sign In</h1>
+            <div className="container login-container">
+                <h1 className="has-text-centered">Sign In</h1>
                 <form>
                     <div className="field py-3">
-                        <label className="label is-size-6 has-text-weight-light">Email</label>
-                        <input type="email" name="email" />
+                        <label className="label">Email</label>
+                        <input className="input" type="email" name="email" />
                     </div>
                     <div className="field">
-                        <label className="label is-size-6 has-text-weight-light">Password</label>
-                        <input type="password" name="password" />
+                        <label className="label">Password</label>
+                        <input className="input" type="password" name="password" />
                     </div>
-                    <input type="submit" value="Login" className="mt-4 is-size-6 button login-button" />
+                    <input type="submit" value="Login" className="mt-4 button login-button" />
                 </form>
                 <div className="or-line py-4">
                     <h1 className="is-size-6 ">OR</h1>
                 </div>
-                <button className="button google-login-button is-size-6">Login with Google</button>
+                <button className="button login-button-google">Login with Google</button>
                 <p className="mt-4 is-size-6 has-text-centered">
-                    Don`t have an account?{' '}
+                    Don&apos;t have an account?{' '}
                     <span className="link has-text-weight-medium" onClick={this.props.onClick}>
-                        Create Your account
+                        <b>Create Your Account.</b>
                     </span>
                 </p>
             </div>
--- src/components/login/SignUpForm.tsx
+++ src/components/login/SignUpForm.tsx
@@ -1,44 +1,44 @@
-import React from 'react';
+import React, { Component, Fragment, ReactNode } from 'react';
 import './LoginForm.scss';
 
 interface ISignUpFormProps {
     onClick: () => void;
 }
 
-class SignUpForm extends React.Component<ISignUpFormProps> {
-    render(): React.ReactNode {
+class SignUpForm extends Component<ISignUpFormProps> {
+    render(): ReactNode {
         return (
-            <div className="sign-in-form">
-                <h1 className="is-size-2 py-3">Create your Account</h1>
+            <div className="container login-container">
+                <p className="mt-4 is-size-6 has-text-centered position-link-registration">
+                    Already have an account?&nbsp;
+                    <span className="link" onClick={this.props.onClick}>
+                        <b>Sign In.</b>
+                    </span>
+                </p>
+                <h1 className="is-size-2 py-3">Create Your Account</h1>
                 <form>
                     <div className="field py-3">
-                        <label className="label is-size-6 has-text-weight-light">Email</label>
-                        <input type="email" name="email" />
+                        <label className="label">Email</label>
+                        <input className="input" type="email" name="email" />
                     </div>
                     <div className="field">
-                        <label className="label is-size-6 has-text-weight-light">Organization Name</label>
-                        <input type="text" name="organization-name" />
+                        <label className="label">Organization Name</label>
+                        <input className="input" type="text" name="organization-name" />
                     </div>
                     <div className="field">
-                        <label className="label is-size-6 has-text-weight-light">School</label>
-                        <input type="text" name="school" />
+                        <label className="label">School</label>
+                        <input className="input" type="text" name="school" />
                     </div>
                     <div className="field">
-                        <label className="label is-size-6 has-text-weight-light">Password</label>
-                        <input type="password" name="password" />
+                        <label className="label">Password</label>
+                        <input className="input" type="password" name="password" />
                     </div>
-                    <input type="submit" value="Create an Account" className="mt-4 is-size-6 button login-button" />
+                    <input type="submit" value="Create an Account" className="mt-4 button login-button" />
                 </form>
                 <div className="or-line py-4">
                     <h1 className="is-size-6 ">OR</h1>
                 </div>
-                <button className="button google-login-button is-size-6">Register with Google</button>
-                <p className="mt-4 is-size-6 has-text-centered position-link-registration">
-                    Already have an account?{' '}
-                    <span className="link has-text-weight-medium" onClick={this.props.onClick}>
-                        Sign In
-                    </span>
-                </p>
+                <button className="button login-button-google">Register with Google</button>
             </div>
         );
     }
