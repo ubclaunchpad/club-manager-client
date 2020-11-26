75167ed330d823e893e8147d65c4f89f4d3a307f
Leonardo Kamino <59890834+LeonardoKamino@users.noreply.github.com>
2020-11-26 09:26:58 -0300

    Update Home page (#62)
    * Update Home page
    
    * Add Sections to home page
    
    * Changed footer content

---
+++ src/components/home/HomeFooter.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import './HomeFooter.scss';
+
+const HomeFooter: FunctionComponent = () => {
+    return (
+        <footer className="home-footer">
+            <p>Created at UBC Launch Pad 2020</p>
+        </footer>
+    );
+};
+
+export default HomeFooter;
