787e618d01be96cb918f3a359288b572dae37f26
Leonardo Kamino <59890834+LeonardoKamino@users.noreply.github.com>
2020-12-01 03:39:24 -0300

    Center forms vertically (#68)

--- src/components/login/SignUpForm.tsx
+++ src/components/login/SignUpForm.tsx
@@ -8,37 +8,39 @@ interface ISignUpFormProps {
 class SignUpForm extends Component<ISignUpFormProps> {
     render(): ReactNode {
         return (
-            <div className="container login-container">
-                <p className="mt-4 is-size-6 has-text-centered position-link-registration">
-                    Already have an account?&nbsp;
-                    <span className="link" onClick={this.props.onClick}>
-                        <b>Sign In.</b>
-                    </span>
-                </p>
-                <h1 className="is-size-2 py-3">Create Your Account</h1>
-                <form>
-                    <div className="field py-3">
-                        <label className="label">Email</label>
-                        <input className="input" type="email" name="email" />
+            <div className="form">
+                <div className="container login-container">
+                    <p className="mt-4 is-size-6 has-text-centered position-link-registration">
+                        Already have an account?&nbsp;
+                        <span className="link" onClick={this.props.onClick}>
+                            <b>Sign In.</b>
+                        </span>
+                    </p>
+                    <h1 className="is-size-2 py-3">Create Your Account</h1>
+                    <form>
+                        <div className="field py-3">
+                            <label className="label">Email</label>
+                            <input className="input" type="email" name="email" />
+                        </div>
+                        <div className="field">
+                            <label className="label">Organization Name</label>
+                            <input className="input" type="text" name="organization-name" />
+                        </div>
+                        <div className="field">
+                            <label className="label">School</label>
+                            <input className="input" type="text" name="school" />
+                        </div>
+                        <div className="field">
+                            <label className="label">Password</label>
+                            <input className="input" type="password" name="password" />
+                        </div>
+                        <input type="submit" value="Create an Account" className="mt-4 button login-button" />
+                    </form>
+                    <div className="or-line py-4">
+                        <h1 className="is-size-6 ">OR</h1>
                     </div>
-                    <div className="field">
-                        <label className="label">Organization Name</label>
-                        <input className="input" type="text" name="organization-name" />
-                    </div>
-                    <div className="field">
-                        <label className="label">School</label>
-                        <input className="input" type="text" name="school" />
-                    </div>
-                    <div className="field">
-                        <label className="label">Password</label>
-                        <input className="input" type="password" name="password" />
-                    </div>
-                    <input type="submit" value="Create an Account" className="mt-4 button login-button" />
-                </form>
-                <div className="or-line py-4">
-                    <h1 className="is-size-6 ">OR</h1>
+                    <button className="button login-button-google">Register with Google</button>
                 </div>
-                <button className="button login-button-google">Register with Google</button>
             </div>
         );
     }
