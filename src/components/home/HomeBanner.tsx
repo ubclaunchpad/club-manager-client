a1bc444b99b7f06b3274746b428d81ad183a60ee
Leonardo Kamino <59890834+LeonardoKamino@users.noreply.github.com>
2021-02-24 18:34:06 -0300

    Bug register redirection (#87)
    * Make register button on home to redirect correct
    
    * Added comment
    
    * Fix ESLint warning

--- src/components/home/HomeBanner.tsx
+++ src/components/home/HomeBanner.tsx
@@ -16,10 +16,22 @@ const HomeBanner: FunctionComponent = () => {
                     <br />
                     <div className="level">
                         <div className="level-left">
-                            <Link to="/login" className="button level-item login-button mr-3">
+                            <Link
+                                to={{
+                                    pathname: '/login',
+                                    state: true, //related to isLogin in Login component
+                                }}
+                                className="button level-item login-button mr-3"
+                            >
                                 Login
                             </Link>
-                            <Link to="/login" className="button level-item register-button ml-3">
+                            <Link
+                                to={{
+                                    pathname: '/login',
+                                    state: false, //related to isLogin in Login component
+                                }}
+                                className="button level-item register-button ml-3"
+                            >
                                 Register
                             </Link>
                         </div>
