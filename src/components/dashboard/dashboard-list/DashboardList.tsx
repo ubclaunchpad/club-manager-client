1e5c106c0d21e959dde015009c0a9a2022ccd827
Leqi(Nancy) Wan <leqiwan@alumni.ubc.ca>
2021-03-01 12:00:08 +0800

    Fixing linting errors 2

--- src/components/dashboard/dashboard-list/DashboardList.tsx
+++ src/components/dashboard/dashboard-list/DashboardList.tsx
@@ -7,120 +7,124 @@ import ApplicantManagementModal from '../../modals/ApplicantManagementModal';
 import './DashboardList.scss';
 
 type DashboardListProps = {
-	viewApplicant: (newCount: number) => void;
-	applicants: any[];
+    viewApplicant: (newCount: number) => void;
+    applicants: any[];
 };
 
 type DashboardListState = {
-	showModal: boolean;
-	name: string;
-	role: string;
-	type: string;
-	status: string;
-	screeningGrade?: number;
-	interviewGrade?: number;
+    showModal: boolean;
+    name: string;
+    role: string;
+    type: string;
+    status: string;
+    screeningGrade?: number;
+    interviewGrade?: number;
 };
 
 class DashboardList extends Component<DashboardListProps, DashboardListState> {
-	// To test the UI - delete when values are fetched from the server
-	constructor(props: any) {
-		super(props);
-		this.state = {
-			showModal: false,
-			name: '',
-			role: '',
-			type: '',
-			status: 'Pending',
-			screeningGrade: 0,
-			interviewGrade: 0
-		};
-	}
+    // To test the UI - delete when values are fetched from the server
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            showModal: false,
+            name: '',
+            role: '',
+            type: '',
+            status: 'Pending',
+        };
+    }
 
-	showModal = (
-		name: string,
-		role: string,
-		type: string,
-		status: string,
-		screeningGrade: number,
-		interviewGrade: number
-	): void => {
-		this.setState({
-			showModal: true,
-			name: name,
-			role: role,
-			type: type,
-			status: status,
-			screeningGrade: screeningGrade,
-			interviewGrade: interviewGrade
-		});
-	};
+    showModal = (
+        name: string,
+        role: string,
+        type: string,
+        status: string,
+        screeningGrade: number,
+        interviewGrade: number,
+    ): void => {
+        this.setState({
+            showModal: true,
+            name: name,
+            role: role,
+            type: type,
+            status: status,
+            screeningGrade: screeningGrade,
+            interviewGrade: interviewGrade,
+        });
+    };
 
-	closeModal = (): void => {
-		this.setState({ showModal: false });
-	};
+    closeModal = (): void => {
+        this.setState({ showModal: false });
+    };
 
-	render(): ReactNode {
-		return (
-			<div className="section dashboard-list">
-				<h1>Unsorted Applicants</h1>
-				<div className="buttons">
-					<span>
-						<DashboardListFilter {...{ title: 'All', count: 150, isActive: true }} />{' '}
-					</span>
-					<span>
-						<DashboardListFilter {...{ title: 'Developers', count: 127, isActive: false }} />
-					</span>
-					<span>
-						<DashboardListFilter {...{ title: 'Designers', count: 23, isActive: false }} />
-					</span>
-				</div>
-				<div className="field">
-					<p className="control has-icons-left">
-						<input className="input" type="text" placeholder="Search applicants" />
-						<span className="icon is-small is-left">
-							<i className="fas fa-search" />
-						</span>
-					</p>
-				</div>
-				<div className="section">
-					<Fragment>
-						{this.props.applicants.map((element, index) => (
-							<DashboardListCard
-								{...element}
-								key={index}
-								count={index}
-								viewApplicant={this.props.viewApplicant}
-								setModalAndType={(type: string) => {
-									console.log(element.role);
-									this.showModal(
-										element.name,
-										element.role,
-										type,
-										element.status,
-										element.screeningGrade,
-										element.interviewGrade
-									);
-								}}
-							/>
-						))}
-					</Fragment>
-				</div>
-				<div>
-					<ApplicantManagementModal
-						type={this.state.type}
-						name={this.state.name}
-						role={this.state.role}
-						closeModal={() => this.closeModal()}
-						isActive={this.state.showModal}
-					/>
-				</div>
-			</div>
-		);
-	}
+    render(): ReactNode {
+        return (
+            <div className="section dashboard-list">
+                <h1>Unsorted Applicants</h1>
+                <div className="buttons">
+                    <span>
+                        <DashboardListFilter {...{ title: 'All', count: 150, isActive: true }} />{' '}
+                    </span>
+                    <span>
+                        <DashboardListFilter {...{ title: 'Developers', count: 127, isActive: false }} />
+                    </span>
+                    <span>
+                        <DashboardListFilter {...{ title: 'Designers', count: 23, isActive: false }} />
+                    </span>
+                </div>
+                <div className="field">
+                    <p className="control has-icons-left">
+                        <input className="input" type="text" placeholder="Search applicants" />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-search"></i>
+                        </span>
+                    </p>
+                </div>
+                <div className="section">
+                    <Fragment>
+                        {this.props.applicants.map((element, index) => (
+                            <DashboardListCard
+                                {...element}
+                                key={index}
+                                count={index}
+                                viewApplicant={this.props.viewApplicant}
+                                setModalAndType={(type: string) => {
+                                    console.log(element.role);
+                                    this.showModal(
+                                        element.name,
+                                        element.role,
+                                        type,
+                                        element.status,
+                                        element.screeningGrade,
+                                        element.interviewGrade,
+                                    );
+                                }}
+                            />
+                        ))}
+                    </Fragment>
+                </div>
+                <div>
+                    <ApplicantManagementModal
+                        type={this.state.type}
+                        name={this.state.name}
+                        role={this.state.role}
+                        closeModal={() => this.closeModal()}
+                        isActive={this.state.showModal}
+                    />
+                </div>
+            </div>
+        );
+    }
 
-	renderDashboardListCards(list: any[]): ReactNode {
-		return <Fragment>{list.map((element, index) => <DashboardListCard {...element} key={index} />)}</Fragment>;
-	}
+    renderDashboardListCards(list: any[]): ReactNode {
+        return (
+            <Fragment>
+                {list.map((element, index) => (
+                    <DashboardListCard {...element} key={index} />
+                ))}
+            </Fragment>
+        );
+    }
 }
 
 export default DashboardList;
--- src/components/dashboard/dashboard-list/DashboardListCard.tsx
+++ src/components/dashboard/dashboard-list/DashboardListCard.tsx
@@ -6,138 +6,138 @@ import { faEnvelope, faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface IDashboardListCardProps {
-	name: string;
-	role: string;
-	status: string;
-	count: number;
-	viewApplicant: (newCount: number) => void;
-	setModalAndType: (type: string) => void;
-	screeningGrade?: number;
-	interviewGrade?: number;
+    name: string;
+    role: string;
+    status: string;
+    count: number;
+    viewApplicant: (newCount: number) => void;
+    setModalAndType: (type: string) => void;
+    screeningGrade?: number;
+    interviewGrade?: number;
 }
 
 const DashboardListCard: React.FunctionComponent<IDashboardListCardProps> = (props: IDashboardListCardProps) => {
-	const applicantStatus = props.status;
-	let decisionComponent, emailComponent, gradeComponent;
-	let applicantCardComponent;
-	switch (applicantStatus) {
-		case 'Screened':
-			decisionComponent = (
-				<div className="level-item">
-					<button className="button button-reject" onClick={() => props.setModalAndType('Reject')}>
-						<FontAwesomeIcon icon={faTimes} />
-					</button>
-					<button className="button button-schedule" onClick={() => props.setModalAndType('Schedule')}>
-						<FontAwesomeIcon icon={faCalendarAlt} />
-					</button>
-				</div>
-			);
-			gradeComponent = (
-				<div className="container grade">
-					<p className="title is-4">{props.screeningGrade}</p>
-					<p className="subtitle is-7">Review</p>
-				</div>
-			);
-			break;
-		case 'Screened: Accepted':
-			break;
-		case 'Scheduled for Interview':
-			gradeComponent = (
-				<div className="container grade">
-					<p className="title is-4">{props.screeningGrade}</p>
-					<p className="subtitle is-7">Review</p>
-				</div>
-			);
-			break;
-		case 'Interviewed':
-			decisionComponent = (
-				<div className="level-item">
-					<button className="button button-reject" onClick={() => props.setModalAndType('Reject')}>
-						<FontAwesomeIcon icon={faTimes} />
-					</button>
-					<button className="button button-accept" onClick={() => props.setModalAndType('Accept')}>
-						<FontAwesomeIcon icon={faCheck} />
-					</button>
-				</div>
-			);
-			gradeComponent = (
-				<div className="columns">
-					<div className="container grade">
-						<p className="title is-4">{props.screeningGrade}</p>
-						<p className="subtitle is-7">Review</p>
-					</div>
+    const applicantStatus = props.status;
+    let decisionComponent, emailComponent, gradeComponent;
+    let applicantCardComponent;
+    switch (applicantStatus) {
+        case 'Screened':
+            decisionComponent = (
+                <div className="level-item">
+                    <button className="button button-reject" onClick={() => props.setModalAndType('Reject')}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </button>
+                    <button className="button button-schedule" onClick={() => props.setModalAndType('Schedule')}>
+                        <FontAwesomeIcon icon={faCalendarAlt} />
+                    </button>
+                </div>
+            );
+            gradeComponent = (
+                <div className="container grade">
+                    <p className="title is-4">{props.screeningGrade}</p>
+                    <p className="subtitle is-7">Review</p>
+                </div>
+            );
+            break;
+        case 'Screened: Accepted':
+            break;
+        case 'Scheduled for Interview':
+            gradeComponent = (
+                <div className="container grade">
+                    <p className="title is-4">{props.screeningGrade}</p>
+                    <p className="subtitle is-7">Review</p>
+                </div>
+            );
+            break;
+        case 'Interviewed':
+            decisionComponent = (
+                <div className="level-item">
+                    <button className="button button-reject" onClick={() => props.setModalAndType('Reject')}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </button>
+                    <button className="button button-accept" onClick={() => props.setModalAndType('Accept')}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </button>
+                </div>
+            );
+            gradeComponent = (
+                <div className="columns">
+                    <div className="container grade">
+                        <p className="title is-4">{props.screeningGrade}</p>
+                        <p className="subtitle is-7">Review</p>
+                    </div>
 
-					<div className="container grade">
-						<p className="title is-4">{props.interviewGrade}</p>
-						<p className="subtitle is-7">Interview</p>
-					</div>
-				</div>
-			);
-			break;
-		case 'Final Decision: Accepted':
-		case 'Final Decision: Rejected':
-			emailComponent = (
-				<div className="level-right">
-					<button className="button button-email" onClick={() => props.setModalAndType('Email')}>
-						<FontAwesomeIcon icon={faEnvelope} />
-					</button>
-				</div>
-			);
-			gradeComponent = (
-				<div className="columns">
-					<div className="container grade">
-						<p className="title is-4">{props.screeningGrade}</p>
-						<p className="subtitle is-7">Review</p>
-					</div>
+                    <div className="container grade">
+                        <p className="title is-4">{props.interviewGrade}</p>
+                        <p className="subtitle is-7">Interview</p>
+                    </div>
+                </div>
+            );
+            break;
+        case 'Final Decision: Accepted':
+        case 'Final Decision: Rejected':
+            emailComponent = (
+                <div className="level-right">
+                    <button className="button button-email" onClick={() => props.setModalAndType('Email')}>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </button>
+                </div>
+            );
+            gradeComponent = (
+                <div className="columns">
+                    <div className="container grade">
+                        <p className="title is-4">{props.screeningGrade}</p>
+                        <p className="subtitle is-7">Review</p>
+                    </div>
 
-					<div className="container grade">
-						<p className="title is-4">{props.interviewGrade}</p>
-						<p className="subtitle is-7">Interview</p>
-					</div>
-				</div>
-			);
-			break;
-		case 'Screened: Rejected':
-		case 'Archived: Rejected':
-			return <div />;
-		default:
-			break;
-	}
-	applicantCardComponent = (
-		<div className="level-right">
-			{decisionComponent}
-			{emailComponent}
-			{gradeComponent}
-			<div>
-				<FontAwesomeIcon icon={faEllipsisV} />
-			</div>
-		</div>
-	);
-	return (
-		<div className="container is-fluid dashboard-list-card">
-			<div className="level">
-				<div className="level-left">
-					<div className="level-item">
-						<span className="icon">
-							<FontAwesomeIcon
-								icon={props.role === 'Designer' ? faPaintBrush : faCode}
-								color={'#444444'}
-							/>
-						</span>
-					</div>
-					<div className="level-item">
-						<div className="container">
-							<p className="dashboard-list-card-name" onClick={() => props.viewApplicant(props.count)}>
-								{props.name}
-							</p>
-							<p className="dashboard-list-card-role">{props.role}</p>
-						</div>
-					</div>
-				</div>
-				{applicantCardComponent}
-			</div>
-		</div>
-	);
+                    <div className="container grade">
+                        <p className="title is-4">{props.interviewGrade}</p>
+                        <p className="subtitle is-7">Interview</p>
+                    </div>
+                </div>
+            );
+            break;
+        case 'Screened: Rejected':
+        case 'Archived: Rejected':
+            return <div />;
+        default:
+            break;
+    }
+    applicantCardComponent = (
+        <div className="level-right">
+            {decisionComponent}
+            {emailComponent}
+            {gradeComponent}
+            <div>
+                <FontAwesomeIcon icon={faEllipsisV} />
+            </div>
+        </div>
+    );
+    return (
+        <div className="container is-fluid dashboard-list-card">
+            <div className="level">
+                <div className="level-left">
+                    <div className="level-item">
+                        <span className="icon">
+                            <FontAwesomeIcon
+                                icon={props.role === 'Designer' ? faPaintBrush : faCode}
+                                color={'#444444'}
+                            />
+                        </span>
+                    </div>
+                    <div className="level-item">
+                        <div className="container">
+                            <p className="dashboard-list-card-name" onClick={() => props.viewApplicant(props.count)}>
+                                {props.name}
+                            </p>
+                            <p className="dashboard-list-card-role">{props.role}</p>
+                        </div>
+                    </div>
+                </div>
+                {applicantCardComponent}
+            </div>
+        </div>
+    );
 };
 
 export default DashboardListCard;
