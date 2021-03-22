89a91c44a2956a02667606780935f53b1f7d60b4
Leonardo Kamino <59890834+LeonardoKamino@users.noreply.github.com>
2021-03-22 19:01:36 -0300

    Add margin to auth (#116)
    * Add margin to auth
    
    * Fix Build error
    
    * Change Background Img to img and text component
    
    * Fix lint-style error

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -5,6 +5,8 @@ import './Login.scss';
 import LoginForm from '../components/login/LoginForm';
 import SignUpForm from '../components/login/SignUpForm';
 
+import LoginImage from '../images/LoginImage.svg';
+
 class Login extends React.Component<RouteComponentProps> {
     state = {
         isLogin: true,
@@ -36,7 +38,12 @@ class Login extends React.Component<RouteComponentProps> {
         return (
             <div className="view login-view">
                 <div className="columns is-desktop">
-                    <div className="column is-6 left-section is-hidden-touch login-image "></div>
+                    <div className="column is-6 left-section is-hidden-touch">
+                        <div>
+                            <img src={LoginImage} alt="" className="login-image" />
+                            <p>Created by a bunch of passionate individuals @ UBC Launch Pad</p>
+                        </div>
+                    </div>
                     <div className="column is-6 right-section flex">{this.renderForm()}</div>
                 </div>
             </div>
