c142422377b4c3b906e0fc34fbc6a8c7d81bbd8a
LeonardoKamino <leo.kamino@gmail.com>
2021-03-22 01:05:18 -0300

    Add description section

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import '../App.scss';
 import './Home.scss';
 import HomeBanner from '../components/home/HomeBanner';
+import DescriptionSection from '../components/home/DescriptionSection';
 
 const Home: React.FunctionComponent = () => {
     return (
         <div className="view home-view">
             <HomeBanner />
+            <DescriptionSection />
         </div>
     );
 };
