8069b9bba70b6169bc226f5ecde4dcc99f002041
LeonardoKamino <leo.kamino@gmail.com>
2021-03-22 15:55:10 -0300

    Add features Icons

--- src/components/home/FeatureCard.tsx
+++ src/components/home/FeatureCard.tsx
@@ -1,24 +1,12 @@
 import React, { FunctionComponent } from 'react';
 import './FeatureCard.scss';
 
-interface IFeatureCardProps {
-    title: string;
-    text: string;
-    imgSrc: string;
-}
+interface IFeatureCardProps {}
 
 const FeatureCard: FunctionComponent<IFeatureCardProps> = (props: IFeatureCardProps) => {
     return (
         <div className="column is-one-third">
-            <div className="feature-card">
-                <figure className="card-image">
-                    <img src={props.imgSrc} alt={props.imgSrc} />
-                </figure>
-                <div className="feature-card-content">
-                    <p className="title">{props.title}</p>
-                    <p className="text">{props.text}</p>
-                </div>
-            </div>
+            <div className="feature-card">test</div>
         </div>
     );
 };
--- src/components/home/FeaturesSection.tsx
+++ src/components/home/FeaturesSection.tsx
@@ -2,23 +2,20 @@ import React, { FunctionComponent } from 'react';
 import './FeaturesSection.scss';
 import './FeatureCard';
 import FeatureCard from './FeatureCard';
-import SheetsIcon from '../../images/SheetsIcon.svg';
-import GmailIcon from '../../images/GmailIcon.svg';
-import ApplicantDataIcon from '../../images/ApplicantDataIcon.svg';
 
 const FeaturesSection: FunctionComponent = () => {
     return (
         <section className="features-section">
-            <p className="slogan">Simple. Integrated. Fast.</p>
-            <h1 className="title mt-3">Features You&apos;ll Love</h1>
-            <div className="columns">
-                <FeatureCard title="Google Sheets Integration" text="No need for manual inputs." imgSrc={SheetsIcon} />
-                <FeatureCard title="Email Automation" text="Only one-click away." imgSrc={GmailIcon} />
-                <FeatureCard
-                    title="View Applicants' Data"
-                    text="All in a single streamlined page."
-                    imgSrc={ApplicantDataIcon}
-                />
+            <p className="feature-slogan">Features that just make sense</p>
+            <p className="feature-header">
+                So, what makes us special?
+                <br /> We’re glad you asked.
+            </p>
+            <div className="columns is-multiline">
+                <FeatureCard />
+                <FeatureCard />
+                <FeatureCard />
+                <FeatureCard />
             </div>
         </section>
     );
