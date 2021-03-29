69007a1760fa08f97d1cbb9e233af30c1fd461b3
Kavpreet Grewal <kavpreetgrewal@gmail.com>
2021-03-28 23:50:27 -0700

    Added logged in check before going to login page

--- src/App.tsx
+++ src/App.tsx
@@ -37,10 +37,7 @@ const PrivateRoute = ({ component: Component, ...rest }: any) => (
 );
 
 const LoginRoute = ({ component: Component, ...rest }: any) => (
-    <Route
-        {...rest}
-        render={(props) => (AuthUtil.checkAuth() ? <Component {...props} /> : <Login {...props} />)}
-    />
+    <Route {...rest} render={(props) => (AuthUtil.checkAuth() ? <Component {...props} /> : <Login {...props} />)} />
 );
 
 export default App;
