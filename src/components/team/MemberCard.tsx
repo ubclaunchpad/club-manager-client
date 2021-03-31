651d5a8e1d52d3d40e4b6325a3db7e9536ca81ed
LeonardoKamino <leo.kamino@gmail.com>
2021-03-30 22:48:44 -0300

    Fix warning compile

--- src/components/team/MemberCard.tsx
+++ src/components/team/MemberCard.tsx
@@ -13,7 +13,7 @@ class MemberCard extends React.Component<IMemberCardProps> {
     render(): React.ReactNode {
         return (
             <div className="column is-3 member-card">
-                <img src={this.props.image} alt="profile picture" className="profile-picture" />
+                <img src={this.props.image} alt="" className="profile-picture" />
                 <p className="name">{this.props.name}</p>
                 <p className="role">{this.props.role}</p>
                 <a className="icon" href={this.props.linkedin} target="blank">
