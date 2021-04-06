d2bcfc99151213bb9448d06556167b56c7ce3578
thomaslapadat <thomaslapadat@gmail.com>
2021-04-05 19:32:26 -0700

    Code now doesn't break when screening grade is missing

--- src/components/dashboard/dashboard-list/DashboardList.tsx
+++ src/components/dashboard/dashboard-list/DashboardList.tsx
@@ -133,7 +133,7 @@ class DashboardList extends Component<DashboardListProps, DashboardListState> {
         }
 
         // Filter by the applicant's screening grade
-        if (applicant.screeningGradeActual.total < minScreen) return false;
+        if (applicant.screeningGradeActual && applicant.screeningGradeActual.total < minScreen) return false;
         // Filter by the applicant's interview grade
         // if (applicant.interviewGradeActual < minInterview) return false;
 
